783fd51199c8e74061d8647129e6376f6af6e1e6
rcalinjageman <rcalinjageman@gmail.com>
2018-05-17 10:03:34 -0500

    Updated trial3h link

--- mirror_trace.js
+++ mirror_trace.js
@@ -1,6 +1,6 @@
 var materials = {
 		'mirror' : [false, true, true, true, true, true, true],
-		'file_names' : ["https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/sample.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh1.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh3.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial1.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh3.png"],
+		'file_names' : ["https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/sample.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh1.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh3.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial1.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial3.png"],
 		'xstarts' : [47,	27,		40,		280,		27, 40, 280],
 		'ystarts' : [256,	275,	45,		276,		275, 45, 276],
 		'xends' :   [344,	370,	368,	33,			370, 368, 33],
