4f652b50af0f9b6c1b2911d5936247091b73372d
Robert J Calin-Jageman <rcalinjageman@gmail.com>
2019-01-11 21:27:43 -0600

    Update mirror_trace.js

--- mirror_trace.js
+++ mirror_trace.js
@@ -16,12 +16,12 @@
 // the images posted on github all have the same total line length and 15 segments 
 
 var materials = {
-		'mirror' : [false, true, true, true, true, true, true],
-		'file_names' : ["https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/sample.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh1.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh3.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial1.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial3.png"],
-		'xstarts' : [47,	27,		40,		280,		27, 40, 280],
-		'ystarts' : [256,	275,	45,		276,		275, 45, 276],
-		'xends' :   [344,	370,	368,	33,			370, 368, 33],
-		'yends' :    [260,	28,		267,	250,		28,267,250]
+		'mirror' : [false, true, true, true, true, true, true, true],
+		'file_names' : ["https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/sample.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh1.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh3.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial1.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial3.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/sample.png"],
+		'xstarts' : [47,	27,		40,		280,		27, 40, 280, 33],
+		'ystarts' : [256,	275,	45,		276,		275, 45, 276, 250],
+		'xends' :   [344,	370,	368,	33,			370, 368, 33, 47],
+		'yends' :    [260,	28,		267,	250,		28,267,250, 256]
 	}
 	
 	//	'xstarts' : [47,	27,		40,		280,		40,		383,	352],
